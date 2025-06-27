4c53dd926220c86066fae4ad7c4f0d03721a4b51
mahadi-sourav <md.mahadi.sourav@optimizely.com>
2025-06-27 23:37:12 +0600

    change gradient color

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -83,12 +83,12 @@ export default function Projects() {
   const gradients = [
     "from-blue-400 to-purple-500",
     "from-green-400 to-blue-500",
-    "from-teal-400 to-cyan-500",
+    "from-purple-400 to-pink-500",
     "from-yellow-400 to-orange-500",
     "from-pink-400 to-red-500",
-    "from-purple-400 to-pink-500",
+    "from-orange-400 to-red-500",
     "from-pink-400 to-orange-500",
-    "from-teal-400 to-red-500",
+    "from-blue-400 to-cyan-500",
     "from-green-400 to-blue-500",
   ];
 
